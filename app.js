048d0c566256d940ae96e3d3102eeecbcc451a9b
zabalueva <age_eva@mail.ru>
2021-03-09 13:35:55 +0300

    feat: add delete and form

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-let titles = Array.from(document.getElementsByClassName('title'));
+const tasks = document.querySelector('.tasks');
 
-titles.forEach(function(item) {
-	console.log(item)
+
+tasks.addEventListener('click', (e) => {
+	if (e.target.className == 'delete') {
+		const li = e.target.parentElement;
+		tasks.removeChild(li)
+	}
+})
+
+
+const addForm = document.forms['add-task'];
+addForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const value = addForm.querySelector('input[type="text"]').value;
+
+	const li = document.createElement('li');
+	const taskName = document.createElement('span');
+	const deleteBtn = document.createElement('span');
+
+	taskName.textContent = value;
+	taskName.classList.add('name');
+
+	deleteBtn.textContent = 'delete';
+	deleteBtn.classList.add('delete');
+
+	li.appendChild(taskName);
+	li.appendChild(deleteBtn);
+	tasks.appendChild(li);
 })
